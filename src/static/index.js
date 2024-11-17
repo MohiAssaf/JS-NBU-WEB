eed949be8af1c35b5324259f2d9c04c56e83e12a
Mohamed Abu assaf <mohamedabuassaf@Mo-2.local>
2024-11-17 20:26:31 +0200

    updated the look of the nbu project so that we can run the pipeline and update the docker image in docker hub

--- src/static/index.js
+++ src/static/index.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    let dockerBtn = document.getElementById('docker-btn');
+    let dockerBtn = document.getElementById('uni-btn');
     let nbuLogo = document.querySelector('.img-logo');
 
     dockerBtn.addEventListener('click', () => {
         if(dockerBtn.classList.contains('clikced')){
             dockerBtn.classList.remove('clikced');
-            dockerBtn.innerHTML = 'Clikc the button to show the image'
+            dockerBtn.innerHTML = 'Clikc the button to show the University'
             nbuLogo.style.display = 'none'
         }else{
             dockerBtn.classList.add('clikced')
